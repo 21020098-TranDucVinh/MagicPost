3addc04ea55fbb84da9eec67369f671acffa1489
xuanbaojr <phanxuanbaoa0@gmail.com>
2023-10-26 12:18:12 +0700

    01

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:5000/giaodichvien/');
+        const data = await response.json();
+        console.log(data)
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Items from MongoDB</h1>
+      <ul>
+        {items.map(item => (
+          <li key={item._id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
